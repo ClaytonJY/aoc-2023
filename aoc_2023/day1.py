ff37666ccc1449135fe4ca2e3dc7f1474fc50ae1
Clayton Yochum <claytonjy@gmail.com>
2023-12-02 16:14:10 -0700

    Solve day 1, part2.

--- aoc_2023/day1.py
+++ aoc_2023/day1.py
@@ -1,5 +1,49 @@
-from pathlib import Path
 import re
+from pathlib import Path
+
+WORD_TO_DIGIT = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+def first_digit(line: str) -> str:
+    """
+    >>> first_digit('two1nine')
+    '2'
+    >>> first_digit('eightwothree')
+    '8'
+    """
+    if line[0].isdigit():
+        return line[0]
+    for word, digit in WORD_TO_DIGIT.items():
+        if line.startswith(word):
+            return digit
+    return first_digit(line[1:])
+
+
+def last_digit(line: str) -> str:
+    """
+    >>> last_digit('two1nine')
+    '9'
+    >>> last_digit('eightwothree')
+    '3'
+    >>> last_digit('threeightwo')
+    '2'
+    """
+    if line[-1].isdigit():
+        return line[-1]
+    for word, digit in WORD_TO_DIGIT.items():
+        if line.endswith(word):
+            return digit
+    return last_digit(line[:-1])
 
 
 class Day1:
@@ -23,7 +67,27 @@ class Day1:
 
         return sum(numbers)
 
+    @staticmethod
+    def part2(lines: list[str]) -> int:
+        """
+        >>> lines = [
+        ...     'two1nine',
+        ...     'eightwothree',
+        ...     'abcone2threexyz',
+        ...     'xtwone3four',
+        ...     '4nineeightseven2',
+        ...     'zoneight234',
+        ...     '7pqrstsixteen',
+        ... ]
+        >>> Day1.part2(lines)
+        281
+        """
+
+        return sum(int(first_digit(line) + last_digit(line)) for line in lines)
+
 
 if __name__ == "__main__":
     lines = Path("inputs/day1.txt").read_text().strip().split("\n")
-    print(Day1.part1(lines))
+
+    assert Day1.part1(lines) == 53080
+    assert Day1.part2(lines) == 53268
